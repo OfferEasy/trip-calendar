4f698e7696f34d578efc19bf59019b10f84b311c
Kyle <offereasy2@gmail.com>
2021-01-24 10:31:24 -0500

    trip-calendar: second commit.

---
+++ src/redux/trip/tripList/reducers.js
@@ -0,0 +1,12 @@
+import Data from "../../../data/data";
+import actions from "./actions";
+
+const initState = {
+    tripsData: []
+};
+
+const reducer = (state = initState, action) => {
+    
+};
+
+export default reducer;
